ae387546f55cc003441528f3f76adacf579976f2
CatDew10 <radosh.yulia@gmail.com>
2024-10-29 17:58:41 -0600

    Bowling interface added.

---
+++ Sources/BowlingApp.Service.AppService/IBowlingService.cs
@@ -0,0 +1,16 @@
+﻿using BowlingApp.Service.Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingApp.Service.AppService;
+
+public interface IBowlingService
+{
+    Game InitializeGame();
+    string RecordDelivery(int gameId, int pinsKnockedDown);
+    FrameScore GetFrameScore(int gameId, int frameNumber);
+    GameScore GetTotalScore(int gameId);
+}
