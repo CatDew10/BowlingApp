26532c7300b5edb9215b20f3ccfbc609003bd54a
CatDew10 <radosh.yulia@gmail.com>
2024-10-31 21:11:01 -0600

    Mocks added. Some samples unit test added for services and APIs

---
+++ UnitTests/BowlingApp.Service.Api.Tests/BowlingControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using BowlingApp.Service.Api.Controllers;
+using BowlingApp.Service.AppService;
+using BowlingApp.Service.Store.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingApp.Service.Api.Tests;
+
+[TestFixture]
+public class BowlingControllerTests
+{
+    private Mock<IBowlingService> _mockBowlingService;
+    private BowlingController _controller;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockBowlingService = new Mock<IBowlingService>();
+        _controller = new BowlingController(_mockBowlingService.Object);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _controller.Dispose();
+    }
+
+    [Test]    
+    public void InitializeGame_ShouldReturnNewGame()
+    {
+        // Arrange
+        var game = new Game { Id = 1 };
+        _mockBowlingService.Setup(s => s.InitializeGame()).Returns(game);
+
+        // Act
+        var result = _controller.InitializeGame();
+
+        // Assert
+        Assert.IsInstanceOf<OkObjectResult>(result.Result);
+        var okResult = result.Result as OkObjectResult;
+        Assert.That(okResult.Value, Is.EqualTo(game));
+        _mockBowlingService.Verify(s => s.InitializeGame(), Times.Once);
+    }
+}
---
+++ UnitTests/BowlingApp.Service.AppService.Tests/BowlingServiceTests.cs
@@ -0,0 +1,116 @@
+﻿using BowlingApp.Service.Store;
+
+namespace BowlingApp.Service.AppService.Tests;
+
+[TestFixture]
+public class BowlingServiceTests
+{
+    private BowlingService _bowlingService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _bowlingService = new BowlingService();
+    }
+
+    [TearDown]
+    public void TearDown() 
+    {
+        StaticRepository.CleareTheGameRepository();    
+    }
+
+
+    [Test]
+    public void InitializeGame_ShouldCreateAndReturnNewGame()
+    {
+        // Act
+        var game = _bowlingService.InitializeGame();
+
+        // Assert
+        Assert.IsNotNull(game);
+        Assert.That(game.Id, Is.EqualTo(1));
+        Assert.IsTrue(StaticRepository.Games.ContainsKey(game.Id));
+
+    }
+
+    [Test]
+    public void RecordDelivery_ShouldRecordFirstDelivery()
+    {
+        // Arrange
+        var game = _bowlingService.InitializeGame();
+
+        // Act
+        var result = _bowlingService.RecordDelivery(game.Id, 7);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("OK"));
+        var frame = game.Frames[game.CurrentFrame - 1];
+        Assert.That(frame.FirstDelivery, Is.EqualTo(7));
+        Assert.IsTrue(frame.IsOpen);
+    }
+
+    [Test]
+    public void RecordDelivery_ShouldRegisterStrikeAndMoveToNextFrame()
+    {
+        // Arrange
+        var game = _bowlingService.InitializeGame();
+
+        // Act
+        var result = _bowlingService.RecordDelivery(game.Id, 10);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("OK"));
+        var frame = game.Frames[0];
+        Assert.IsTrue(frame.IsStrike);
+        Assert.That(game.CurrentFrame, Is.EqualTo(2));  // Moved to next frame
+    }
+
+    [Test]
+    public void RecordDelivery_ShouldRegisterSpareWhenFrameAddsUpToTen()
+    {
+        // Arrange
+        var game = _bowlingService.InitializeGame();
+
+        // Act
+        _bowlingService.RecordDelivery(game.Id, 5);
+        var result = _bowlingService.RecordDelivery(game.Id, 5);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("OK"));
+        var frame = game.Frames[0];
+        Assert.IsTrue(frame.IsSpare);
+        Assert.IsFalse(frame.IsOpen);
+    }
+
+    [Test]
+    public void GetFrameScore_ShouldReturnCorrectScoreForGivenFrame()
+    {
+        // Arrange
+        var game = _bowlingService.InitializeGame();
+        _bowlingService.RecordDelivery(game.Id, 5);
+        _bowlingService.RecordDelivery(game.Id, 3);
+
+        // Act
+        var frameScore = _bowlingService.GetFrameScore(game.Id, 1);
+
+        // Assert
+        Assert.That(frameScore.FrameTotal, Is.EqualTo(8));
+        Assert.That(frameScore.FrameNumber, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void GetTotalScore_ShouldReturnCorrectTotalScore()
+    {
+        // Arrange
+        var game = _bowlingService.InitializeGame();
+        _bowlingService.RecordDelivery(game.Id, 10); // Strike
+        _bowlingService.RecordDelivery(game.Id, 5);
+        _bowlingService.RecordDelivery(game.Id, 4);
+
+        // Act
+        var gameScore = _bowlingService.GetTotalScore(game.Id);
+
+        // Assert
+        Assert.That(gameScore.TotalScore, Is.EqualTo(28));
+    }
+}
